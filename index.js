432b87fe0c68ea98d470aa5a65a8cb8dc0e3803a
Muhammad Irfan <m.irfan@bukalapak.com>
2018-03-12 14:28:30 +0700

    change compiler

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var compiler = require('vue-template-compiler');
+var compiler = require('vue-docgen-api');
 
 exports.handlers = {
   beforeParse: function (e) {
     if (/\.vue$/.test(e.filename)) {
-      var output = compiler.parseComponent(e.source);
+      var output = compiler.parse(e.source);
+
+      console.log("====================")
+      console.log(output)
+      console.log("====================")
 
       e.source = output.script ? output.script.content : '';
     }
